0e512a4ee2eed415e567d6f543fe56d10989b10d
Arthur <arthuroaraujo@gmail.com>
2022-06-22 18:14:04 -0300

    Exercicios 4.2

---
+++ fundamentos/4-introdução-a-javascript-e-logica-de-programacao/dia-1/exercicio_2.js
@@ -0,0 +1,8 @@
+const primeiroNumero = 10;
+const segundoNumero = 20;
+
+if(primeiroNumero > segundoNumero) {
+    console.log('primeiroNumero é maior que segundoNumero');
+} else {
+    console.log ('segundoNumero é maior que primeiroNumero');
+};
